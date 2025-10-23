8300d066cedcaea12c5b690eb38203fdb1b63bac
ezrafil <rofiherawan@gmail.com>
2025-10-23 12:59:35 +0700

    add header (belum jadi)

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gitar Surabaya</title> <link rel="stylesheet" href="css/style.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<header class="site-header">
+    <div class="container">
+        
+        <div class="logo">
+            <a href="index.php">
+                <img src="images/logo-1.png" alt="Logo Gitar Surabaya"> 
+            </a>
+        </div>
+
+        <nav class="main-nav">
+            <ul>
+                <li><a href="tentang.php">Tentang Kami</a></li>
+                <li><a href="layanan.php">Layanan</a></li>
+                <li><a href="galeri.php">Galeri</a></li>
+            </ul>
+        </nav>
+
+    </div>
+</header>
