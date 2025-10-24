e69ac45fd7f0c1e9352b4875a74883ae58899272
ezrafil <rofiherawan@gmail.com>
2025-10-24 15:25:22 +0700

    header done (proses content)

--- index.php
+++ index.php
@@ -1,32 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gitar Surabaya</title> <link rel="stylesheet" href="css/style.css">
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gitar Surabaya</title>
+
+  <!-- Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-<header class="site-header">
+  <!-- HEADER -->
+  <header class="site-header">
     <div class="container">
-        
-        <div class="logo">
-            <a href="index.php">
-                <img src="images/logo-1.png" alt="Logo Gitar Surabaya"> 
-            </a>
-        </div>
 
-        <nav class="main-nav">
-            <ul>
-                <li><a href="tentang.php">Tentang Kami</a></li>
-                <li><a href="layanan.php">Layanan</a></li>
-                <li><a href="galeri.php">Galeri</a></li>
-            </ul>
-        </nav>
+      <!-- Logo kiri -->
+      <div class="logo">
+        <a href="#">
+          <img src="./images/logo-1.png" alt="Logo Gitar Surabaya">
+        </a>
+      </div>
+
+      <!-- Navbar tengah -->
+      <nav class="main-nav">
+        <ul>
+          <li><a href="#tentang">Tentang Kami</a></li>
+          <li><a href="#layanan">Layanan</a></li>
+          <li><a href="#galeri">Galeri</a></li>
+        </ul>
+      </nav>
 
     </div>
-</header>
+  </header>
+
+</body>
+</html>
