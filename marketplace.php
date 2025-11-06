2010b6f39ec68ed1c3e59e9217d0fe06ffe8200d
mohammadrayhanrizki <mohammadrayhanrizki@gmail.com>
2025-11-06 10:26:07 +0700

    rename title

--- marketplace.php
+++ marketplace.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marketplace | Gitar Surabaya</title>
+    <title>Gitar Surabaya</title>
     <link rel="icon" type="image/png" href="./images/logo.png">
     <!-- FONT & STYLES -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
